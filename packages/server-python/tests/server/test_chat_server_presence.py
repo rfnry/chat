38263b0b5052ddcb9a4724710ac6e686b6c0d732
Fernando Vargas <usuario.frndvrgs@gmail.com>
2026-04-23 03:21:04 -0300

    test(server): assert each ChatServer has an independent PresenceRegistry

--- packages/server-python/tests/server/test_chat_server_presence.py
+++ packages/server-python/tests/server/test_chat_server_presence.py
@@ -4,3 +4,9 @@ from rfnry_chat_server import ChatServer, InMemoryChatStore, PresenceRegistry
 def test_chat_server_exposes_presence_registry():
     server = ChatServer(store=InMemoryChatStore())
     assert isinstance(server.presence, PresenceRegistry)
+
+
+def test_each_chat_server_has_independent_presence_registry():
+    a = ChatServer(store=InMemoryChatStore())
+    b = ChatServer(store=InMemoryChatStore())
+    assert a.presence is not b.presence
